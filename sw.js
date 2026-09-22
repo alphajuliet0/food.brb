860783b3d05080aa25286d5ea36dd56578c3c171
Ajay Panchal <ajaypanch@gmail.com>
2026-09-22 10:46:08 +0000

    Add collapsible profile and settings shell

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // food.brb service worker. Cache cleanup never touches localStorage.
-const CACHE='food-brb-v11-2-compact-stats';
+const CACHE='food-brb-v12-app-shell';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./xlsx.mini.min.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
