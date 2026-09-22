ce1c2464c41121b809413203e4c66a374a3a88a1
Ajay Panchal <ajaypanch@gmail.com>
2026-09-22 09:38:29 +0000

    Polish empty meal add controls

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // food.brb service worker. Cache cleanup never touches localStorage.
-const CACHE='food-brb-v9-pt-import';
+const CACHE='food-brb-v9-1-diary-polish';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./xlsx.mini.min.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
