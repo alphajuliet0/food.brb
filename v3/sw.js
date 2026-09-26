8a10e5fec18e876598a7fc6ade33db1076f0fee1
Ajay Panchal <ajaypanch@gmail.com>
2026-09-26 13:44:02 +0100

    Update cache version to food-brb-v3-a4

--- v3/sw.js
+++ v3/sw.js
@@ -1,4 +1,4 @@
-const CACHE='food-brb-v3-a3';
+const CACHE='food-brb-v3-a4';
 self.addEventListener('install',e=>{self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(clients.claim());});
 self.addEventListener('fetch',e=>{
