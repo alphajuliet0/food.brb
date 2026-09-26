79d294c986a5969f4b2acc4a08374af757616eaa
Ajay Panchal <ajaypanch@gmail.com>
2026-09-26 17:39:13 +0100

    Update sw.js

--- v3/sw.js
+++ v3/sw.js
@@ -1,4 +1,4 @@
-const CACHE='food-brb-v3-a19';
+const CACHE='food-brb-v3-a20';
 self.addEventListener('install',e=>{self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(clients.claim());});
 self.addEventListener('fetch',e=>{
