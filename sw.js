c5c66ceb2abff3adae5441cc4d3ba4085bb27363
Ajay Panchal <ajaypanch@gmail.com>
2026-09-26 17:50:21 +0100

    Update cache version in service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // food.brb service worker. Cache cleanup never touches localStorage.
-const CACHE='food-brb-live-28';
+const CACHE='food-brb-live-29';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./fonts.css','./data.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
