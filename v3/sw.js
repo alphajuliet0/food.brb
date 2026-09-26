af2ae9e219970451165e0de32690fb6e964eb359
Ajay Panchal <ajaypanch@gmail.com>
2026-09-26 15:05:06 +0100

    Update service worker cache version to v3-a18

--- v3/sw.js
+++ v3/sw.js
@@ -1,4 +1,4 @@
-const CACHE='food-brb-v3-a17';
+const CACHE='food-brb-v3-a18';
 self.addEventListener('install',e=>{self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(clients.claim());});
 self.addEventListener('fetch',e=>{
